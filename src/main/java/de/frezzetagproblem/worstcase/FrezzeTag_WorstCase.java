ca005aacbdf1a195c3648f01fb1fc92113428775
Joseph Alzieb <Joseph.Alzieb@hhu.de>
2024-07-25 00:18:44 +0200

    worst case

---
+++ src/main/java/de/frezzetagproblem/worstcase/FrezzeTag_WorstCase.java
@@ -0,0 +1,161 @@
+package de.frezzetagproblem.worstcase;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.stream.JsonReader;
+import de.frezzetagproblem.Properties;
+import de.frezzetagproblem.optimal.Pair;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
+
+public class FrezzeTag_WorstCase {
+
+  public static void main(String[] args) throws IOException {
+    runExperiments(Properties.ROBOTS_COUNT,8, Properties.OFFSET);
+  }
+
+  private static void runExperiments(int robotsCount, int totalRobotsCount, int offset) throws IOException {
+    Gson gson = new GsonBuilder().setPrettyPrinting().create();
+
+    while (robotsCount <= totalRobotsCount) {
+      Map<String, Double> results = new HashMap<>();
+      Path dir = Paths.get("dummy-data/" + robotsCount);
+      if (!Files.exists(dir)) {
+        Files.createDirectories(dir);
+      }
+
+      DirectoryStream<Path> stream = Files.newDirectoryStream(dir, "*.json");
+
+      /**
+       * Wir lesen alle Ordner in /dummy-dta/ eins nach dem anderen.
+       */
+      for (Path entry : stream) {
+        List<Robot_WorstCase> off = new ArrayList<>();
+        List<Robot_WorstCase> on = new ArrayList<>();
+
+        JsonReader reader = new JsonReader(new FileReader(entry.toFile()));
+        JsonObject jsonObject = gson.fromJson(reader, JsonObject.class);
+        JsonObject robotsJson = jsonObject.getAsJsonObject("robots");
+
+        for (Map.Entry<String, JsonElement> robotEntry : robotsJson.entrySet()) {
+          JsonObject robotObj = robotEntry.getValue().getAsJsonObject();
+          Robot_WorstCase r = gson.fromJson(robotObj, Robot_WorstCase.class);
+
+          if (r.isAktive()) {
+            on.add(r);
+          } else {
+            off.add(r);
+          }
+        }
+
+        double timeunit = 0;
+        List<Double> timeUnits = new ArrayList<>();
+        List<Double> timeUnits_puffer = new ArrayList<>();
+        while (!off.isEmpty()) {
+
+          List<List<Robot_WorstCase>> permutations = generatePermutations(on);
+          TreeMap<Pair<String, String>, Double> possibleSolutions  = new TreeMap<>();
+
+          for (List<Robot_WorstCase> permutation : permutations) {
+            for (Robot_WorstCase r : permutation) {
+              r.computeDistance(off,possibleSolutions);
+            }
+            timeUnits_puffer.clear();
+          }
+
+          for (Robot_WorstCase r : on) {
+            r.run(off, timeUnits, possibleSolutions);
+          }
+
+          /**
+           * ON- und OFF-Listen werden aktualisiert.
+           */
+          for (Iterator<Robot_WorstCase> iterator = off.iterator(); iterator.hasNext(); ) {
+            Robot_WorstCase robot = iterator.next();
+            if (robot.isAktive()) {
+              on.add(robot);
+              iterator.remove();
+            }
+          }
+
+          /**
+           * Nach jedem Schritt wird die Zeit (Endergebnis) aktualisiert, und
+           * die Liste der TimeUnits für den nächsten Durchlauf geleert.
+           */
+          timeunit += getMaxValue(timeUnits);
+          timeUnits.clear();
+        }
+
+        results.put(entry.getFileName().toString(), timeunit);
+        System.out.println("-------------------------------------------------");
+      }
+
+      saveResults(robotsCount, gson, results);
+
+      if (robotsCount < 10){
+        robotsCount++;
+      } else if (robotsCount < 100){
+        robotsCount += 5;
+      } else {
+        robotsCount += 50;
+      }
+    }
+  }
+
+  private static double getMaxValue(List<Double> list) {
+    return Collections.max(list);
+  }
+
+  private static double getMinValue(List<Double> list) {
+    return Collections.min(list);
+  }
+
+  public static <T> List<List<T>> generatePermutations(List<T> list) {
+    List<List<T>> lists = new LinkedList<>();
+    permute(list, 0, lists);
+    return lists;
+  }
+
+  private static <T> void permute(List<T> list, int start, List<List<T>> lists) {
+    if (start >= list.size() - 1) {
+      lists.add(new ArrayList<>(list));
+      return;
+    }
+
+    for (int i = start; i < list.size(); i++) {
+      Collections.swap(list, start, i);
+      permute(list, start + 1, lists);
+      Collections.swap(list, start, i); // Rückgängig machen des Tauschs
+    }
+  }
+
+  private static void saveResults(int robotCount, Gson gson, Map<String, Double> results)
+      throws IOException {
+    String resultDirectory= "results/worstCase/";
+    File resDir = new File(resultDirectory);
+    if (!resDir.exists()) {
+      resDir.mkdirs();
+    }
+    String resultFileName =  resultDirectory + robotCount + "-results.json";
+    try (FileWriter writer = new FileWriter(resultFileName)) {
+      gson.toJson(results, writer);
+    }
+  }
+
+}
---
+++ src/main/java/de/frezzetagproblem/worstcase/Robot_WorstCase.java
@@ -0,0 +1,183 @@
+package de.frezzetagproblem.worstcase;
+
+import de.frezzetagproblem.Location;
+import de.frezzetagproblem.Properties;
+import de.frezzetagproblem.Status;
+import de.frezzetagproblem.optimal.Pair;
+import java.util.List;
+import java.util.Objects;
+import java.util.TreeMap;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+/**
+ * Robot
+ * @Author Joseph Alzieb
+ */
+public class Robot_WorstCase {
+
+  private String id;
+  private Location location;
+  private Status status;
+  private boolean declared;
+  private int velocity;
+  private Robot_WorstCase targetRobot;
+
+  public Robot_WorstCase(String id, Location location, boolean declared) {
+    this.id = id;
+    this.location = location;
+    this.declared = declared;
+    this.status = Status.OFF;
+  }
+
+  public Robot_WorstCase() {
+  }
+
+  public void run(List<Robot_WorstCase> off, List<Double> time,
+      TreeMap<Pair<String, String>, Double> possibleSolutions) {
+    this.targetRobot = this.getNearestRobot(off);
+    if (this.targetRobot != null) {
+      double distance = this.distance(this.targetRobot);
+
+      if (existsWorstSolution(possibleSolutions, distance).get()){
+        this.removeTargetRobot();
+        return;
+      }
+      this.targetRobot.declare();
+      this.aktive(targetRobot);
+      time.add(distance);
+
+      //Der Roboter bewegt sich zu dem zu aktivierenden Robot.
+      this.moveTo(this.targetRobot.location);
+      this.removeTargetRobot();
+    }
+  }
+
+  private AtomicBoolean existsWorstSolution(TreeMap<Pair<String, String>, Double> possibleSolutions, Double distance){
+    AtomicBoolean b = new AtomicBoolean(false);
+    possibleSolutions.forEach((x,y)->{
+      if (this.targetRobot.id.equals(x.getSecond()) && y > distance){
+        b.set(true);
+        return;
+      }
+    });
+
+    return b;
+  }
+
+  public void computeDistance(List<Robot_WorstCase> off,
+      TreeMap<Pair<String, String>, Double> possibleSolutions) {
+    var targetRobot = this.getNearestRobot(off);
+    if (targetRobot != null) {
+      possibleSolutions.put(getKey(targetRobot), this.distance(targetRobot));
+    }
+  }
+
+  private Pair<String, String> getKey(Robot_WorstCase targetRobot) {
+    return new Pair (this.id, targetRobot.id);
+  }
+
+  //TODO Für WorstCase müssen wir was anderes überlegen!!
+  public Robot_WorstCase getNearestRobot(List<Robot_WorstCase> off) {
+    Robot_WorstCase nearest = null;
+    for (Robot_WorstCase r : off) {
+      if (!r.isDeclared() && nearest == null) {
+        nearest = r;
+      } else if (!r.isDeclared() && nearest != null) {
+        if (this.distance(nearest) > this.distance(r)) {
+          nearest = r;
+        }
+      }
+    }
+    return nearest;
+  }
+
+  /**
+   * Die Methode berechnet den Abstand zwischen zwei Punkten.
+   * Je nachdem welche Metrik in {@link Properties} (L1, oder L2) gesetzt ist, wird diese Metrik zur Berechnung
+   * der Distanz verwendet.
+   * @param that other Robot
+   * @return Distance from Robot (this) to Robot (that)
+   */
+  public double distance(Robot_WorstCase that) {
+    if (Properties.METRIK.equals(Properties.L_1)) {
+      return Math.abs(this.location.x - that.location.x) + Math.abs(this.location.y - that.location.y);
+    } else {
+      return Math.sqrt(Math.pow(this.location.x - that.location.x, 2) + Math.pow(this.location.y - that.location.y, 2));
+    }
+
+  }
+
+  public String getId() {
+    return id;
+  }
+
+  public Status getStatus() {
+    return status;
+  }
+
+  public boolean isAktive() {
+    return this.status == Status.ON;
+  }
+
+  public void aktive(Robot_WorstCase targetRobot) {
+    targetRobot.status = Status.ON;
+    System.out.println(this.id + " aktiviert " + targetRobot.id);
+  }
+
+  //von The Visibility Freeze-Tag Problem (FTP in Euclidean Space)
+  public boolean isDeclared() {
+    return this.declared;
+  }
+
+  public void declare() {
+    this.declared = true;
+  }
+
+  public boolean hasTargetRobot(){
+    return this.targetRobot != null;
+  }
+
+  public void removeTargetRobot(){
+    this.targetRobot = null;
+  }
+
+  public int getLocation_x(){
+    return this.location.x;
+  }
+
+  public int getLocation_y(){
+    return this.location.y;
+  }
+
+  public void moveTo(Location location){
+    this.location = location;
+  }
+  @Override
+  public String toString() {
+    return "Robot{" +
+        "id='" + id + '\'' +
+        ", location=" + location +
+        ", status=" + status +
+        ", declared=" + declared +
+        ", velocity=" + velocity +
+        ", targetRobot=" + targetRobot +
+        '}';
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) {
+      return true;
+    }
+    if (!(o instanceof Robot_WorstCase robot)) {
+      return false;
+    }
+    return Objects.equals(id, robot.id) && Objects.equals(location,
+        robot.location);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(id, location);
+  }
+}
