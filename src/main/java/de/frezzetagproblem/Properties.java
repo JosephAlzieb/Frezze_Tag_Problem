efff5bf117015b64998f924b775afc89febb7f27
Joseph Alzieb <Joseph.Alzieb@hhu.de>
2024-08-11 15:22:10 +0200

    compare all_possible_solutions on normal-data and on-edge-data.

--- src/main/java/de/frezzetagproblem/Properties.java
+++ src/main/java/de/frezzetagproblem/Properties.java
@@ -43,7 +43,7 @@ public class Properties {
 
   public static String NORMAL_CASE_RESULT_FILE_NAME = "results/normal/";
 
-  public static boolean ALLOW_GENERATE_WORSTCASE_DATA = true;
+  public static boolean ALLOW_GENERATE_WORSTCASE_DATA = false;
 
   public static boolean ALLOW_MULTIPLE_ROBOTS = false;
 
